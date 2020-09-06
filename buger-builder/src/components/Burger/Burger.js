75a2fe9dff82a28f8748f02a1aecbcb8f366be28
Kevin Péraldi <peraldi.kevin@gmail.com>
2020-09-06 18:00:17 +0200

    render different ingredients

--- buger-builder/src/components/Burger/Burger.js
+++ buger-builder/src/components/Burger/Burger.js
@@ -3,12 +3,15 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 
-const Burger = () => {
+const Burger = (props) => {
+    const ingredients = Object.keys(props.ingredients)
+                              .map(ingredientKey => [...Array(props.ingredients[ingredientKey])]
+                                    .map((_, index) => <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />));
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {ingredients}
             <BurgerIngredient type="bread-bottom" />            
         </div>
     );
