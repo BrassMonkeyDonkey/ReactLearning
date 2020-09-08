57dc5293c63b5eb652f0af71d59d64ff5eb829ea
Kevin Peraldi <kperaldi@GENETEC.COM>
2020-09-08 18:37:19 +0200

    add build control mapping

--- buger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ buger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -3,9 +3,18 @@ import React from 'react';
 import classes from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl';
 
-const buildControls = (props) = (
-    <div>
-        
+const controls = [
+    { Label: "Salad", Type: "salad" },
+    { Label: "Cheese", Type: "cheese" },
+    { Label: "Bacon", Type: "bacon" },
+    { Label: "Meat", Type: "meat" }    
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {controls.map((ctrl) => {
+            return <BuildControl key={ctrl.Type} label={ctrl.Label} />
+        })}
     </div>
 );
 
