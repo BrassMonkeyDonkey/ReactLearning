c4460368c4a2db8f22ffad7c3340029a557cf3dc
Kevin Péraldi <peraldi.kevin@gmail.com>
2020-09-12 11:33:50 +0200

    add total price to summary

--- buger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ buger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,38 @@
-import React from 'react';
+import React from "react";
 
-import Aux from '../../../hoc/Auxiliary';
+import Aux from "../../../hoc/Auxiliary";
 import Button from "../../UI/Button/Button";
 
-const buttonTypes = [
-    "Danger",
-    "Success"
-]
+const buttonTypes = ["Danger", "Success"];
 
 const OrderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-        return (
-            <li>
-                <p><stan style={{textTransform: 'capitalized'}}>{igKey}:</stan> {props.ingredients[igKey]}</p>
-            </li>
-        )
-    })
-
+  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
     return (
-        <Aux>
-            <h3>Your order</h3>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Continue to checkout?</p>
-            <Button btnType={buttonTypes[0]} clicked={props.purchaseCancelled} >CANCEL</Button>
-            <Button btnType={buttonTypes[1]} clicked={props.purchaseContinued} >CONTINUE</Button>
-        </Aux>        
-    )
-}
+      <li>
+        <p>
+          <stan style={{ textTransform: "capitalized" }}>{igKey}:</stan>{" "}
+          {props.ingredients[igKey]}
+        </p>
+      </li>
+    );
+  });
+
+  return (
+    <Aux>
+      <h3>Your order</h3>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total price: {props.price.toFixed(2)}$</strong>
+      </p>
+      <p>Continue to checkout?</p>
+      <Button btnType={buttonTypes[0]} clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType={buttonTypes[1]} clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </Aux>
+  );
+};
 
 export default OrderSummary;
