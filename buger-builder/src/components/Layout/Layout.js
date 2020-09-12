09345729827c65e9399487505514002a57d808b5
Kevin Péraldi <peraldi.kevin@gmail.com>
2020-09-12 11:49:55 +0200

    add toolbar

--- buger-builder/src/components/Layout/Layout.js
+++ buger-builder/src/components/Layout/Layout.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import Auxiliary from '../../hoc/Auxiliary';
-import classes from './Layout.module.css'
+import Auxiliary from "../../hoc/Auxiliary";
+import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const Layout = (props) => (
-    <Auxiliary>
-        <div>Toolbar & SideDrawer</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxiliary>
+  <Auxiliary>
+    <Toolbar />
+    <main className={classes.Content}>{props.children}</main>
+  </Auxiliary>
 );
 
 export default Layout;
