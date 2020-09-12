713fe54168b0bf5f32c0770de22417d4c887b30b
Kevin Péraldi <peraldi.kevin@gmail.com>
2020-09-12 12:01:01 +0200

    add logo img

--- buger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ buger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar} >
-        <div>Menu</div>
-        <div>Logo</div>
-        <nav>
-            ...
-        </nav>
-    </header>
+  <header className={classes.Toolbar}>
+    <div>Menu</div>
+    <Logo />
+    <nav>...</nav>
+  </header>
 );
 
 export default toolbar;
