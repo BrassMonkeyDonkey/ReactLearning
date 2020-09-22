8a6737e0755ae90aed8502c4908edb6cc4fba0ad
Kevin Peraldi <kperaldi@GENETEC.COM>
2020-09-22 18:18:02 +0200

    add spinner

--- buger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ buger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import Spinner from "../../components/UI/Spinner/Spinner";
 import axios from "../../axios-orders";
 
 const INGREDIENTS_PRICE = {
@@ -25,6 +26,7 @@ class BurgerBuilder extends Component {
     totalPrice: 4,
     purchasable: false,
     purchased: false,
+    loading: false
   };
 
   updatePurchasableState(Ingredients) {
@@ -76,6 +78,8 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
+    this.setState({loading: true});
+
     const order = {
       ingredients: this.state.ingredients,
       price: this.state.totalPrice,
@@ -91,9 +95,10 @@ class BurgerBuilder extends Component {
       deliveryMethod: "fastest"
     }
 
+    // the ".json" is requiered by Firebase. It is how it is working to automatically handle new "document" creation.
     axios.post("/orders.json/", order)
-    .then(response => console.log(response))
-    .catch(error => console.log(error));
+    .then(response => this.setState({loading: false, purchased: false}))
+    .catch(error => this.setState({loading: false, purchased: false}));
   };
 
   render() {
@@ -105,17 +110,22 @@ class BurgerBuilder extends Component {
       disabledInfo[key] = disabledInfo[key] <= 0;
     }
 
+    let orderSummary = <OrderSummary
+    ingredients={this.state.ingredients}
+    price={this.state.totalPrice}
+    purchaseContinued={this.purchaseContinueHandler}
+    purchaseCancelled={this.purchaseCancelHandler} />;
+    if(this.state.loading) {
+      orderSummary = <Spinner />;
+    }
+
     return (
       <Auxiliary>
         <Modal
           show={this.state.purchased}
           modalClosed={this.purchaseCancelHandler}
         >
-          <OrderSummary
-            ingredients={this.state.ingredients}
-            price={this.state.totalPrice}
-            purchaseContinued={this.purchaseContinueHandler}
-            purchaseCancelled={this.purchaseCancelHandler} />
+         {orderSummary}
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
