da4a24c7cc7c3e2f3096cf0ef372c920de9276be
Kevin Peraldi <kperaldi@GENETEC.COM>
2020-09-22 17:57:17 +0200

    persist orders to firebase

--- buger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ buger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENTS_PRICE = {
   salad: 0.5,
@@ -75,7 +76,24 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continued!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Kevin Peraldi",
+        address: {
+          street: "Rue Infroit",
+          zipCode: "78570",
+          country: "France"
+        },
+        email: "kperaldi@mail.com"
+      },
+      deliveryMethod: "fastest"
+    }
+
+    axios.post("/orders.json/", order)
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
   };
 
   render() {
