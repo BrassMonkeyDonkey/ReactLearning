28c0bcf1b98e8cab21a10e5a86c27703efe4b8f3
Kevin Péraldi <peraldi.kevin@gmail.com>
2020-10-11 14:12:44 +0200

    Replace href by Link

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import "./Blog.css";
 
-import Posts from "../../containers/Blog/Posts/Posts";
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
   render() {
@@ -11,12 +12,13 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New post</a></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/new-post">New post</Link></li>
             </ul>
           </nav>
         </header>
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
