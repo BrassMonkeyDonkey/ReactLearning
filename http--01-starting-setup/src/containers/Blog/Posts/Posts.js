ee2eef7b00b73d30069e143c37e9765d43be42b7
Kevin Péraldi <peraldi.kevin@gmail.com>
2020-10-11 14:50:29 +0200

    add full post routing

--- http--01-starting-setup/src/containers/Blog/Posts/Posts.js
+++ http--01-starting-setup/src/containers/Blog/Posts/Posts.js
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import axios from "../../../axios";
 
-import "./Posts.css"
+import "./Posts.css";
 import Post from "../../../components/Post/Post";
 
 class Posts extends Component {
-    state = {
-        posts: []
-      };
+  state = {
+    posts: [],
+  };
 
-      postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-      }
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
 
-      componentDidMount() {
-        axios.get("/posts").then((response) => {
-            const posts = response.data.slice(0, 4);
-            var updatedPosts = posts.map(post => {
-                return {
-                    ...post,
-                    author: "Kevin"
-                }
-            })
-          this.setState({ posts: updatedPosts });
-        })
-        .catch(error => console.log(error));
-      }
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        var updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: "Kevin",
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch((error) => console.log(error));
+  }
 
   render() {
     let posts = (
@@ -35,12 +38,13 @@ class Posts extends Component {
     );
     if (!this.state.error) {
       posts = this.state.posts.map((post) => (
-        <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-        />
+        <Link key={post.id} to={"/" + post.id}>
+          <Post
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        </Link>
       ));
     }
 
